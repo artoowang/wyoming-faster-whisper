2098bc92256fd42b9d799c08ffbd7c9f992afe54
Chunpo Wang <1505070+artoowang@users.noreply.github.com>
2023-12-27 05:00:42 -0800

    Changed to send audio data in chunks

--- test/test_wyoming.py
+++ test/test_wyoming.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Generator
 from wyoming.client import AsyncTcpClient
 from wyoming.event import Event
 
@@ -9,6 +9,24 @@ import whisper
 
 WYOMING_IP = '127.0.0.1'
 WYOMING_PORT = 10300
+SAMPLE_RATE = 16000
+
+def get_test_data() -> Generator[bytes, None, None]:
+    CHUNK_LENGTH_IN_SECONDS = 1
+    CHUNK_SAMPLES = CHUNK_LENGTH_IN_SECONDS * SAMPLE_RATE
+
+    audio_data_fp32 = whisper.load_audio('play-recording.wav')
+    assert audio_data_fp32.dtype == np.float32
+    assert len(audio_data_fp32.shape) == 1
+    print(f'Loaded {audio_data_fp32.size} samples of audio data.')
+
+    audio_data_int16 = (audio_data_fp32 * 32768.0).astype(np.int16)
+    cur_sample = 0
+    while cur_sample < audio_data_int16.size:
+        end_sample = min(cur_sample + CHUNK_SAMPLES, audio_data_int16.size)
+        audio_data = audio_data_int16[cur_sample:end_sample].tobytes()
+        cur_sample = end_sample
+        yield audio_data
 
 async def run():
     async with AsyncTcpClient(WYOMING_IP, WYOMING_PORT) as client:
@@ -16,20 +34,16 @@ async def run():
         result = await client.read_event()
         print(result)
 
-        audio_data_fp32 = whisper.load_audio('play-recording.wav')
-        assert audio_data_fp32.dtype == np.float32
-        assert len(audio_data_fp32.shape) == 1
-        audio_data = (audio_data_fp32 * 32768.0).astype(np.int16).tobytes()
-        print(f'Loaded {len(audio_data)} bytes audio data.')
-
         await client.write_event(Event('transcibe', {'language': 'en'}))
         audio_metadata = {
-            'rate': 16000,
+            'rate': SAMPLE_RATE,
             'width': 2,
             'channels': 1,
         }
         await client.write_event(Event('audio-start', audio_metadata))
-        await client.write_event(Event('audio-chunk', audio_metadata, audio_data))
+        for audio_data in get_test_data():
+            print(f'Write {len(audio_data)} bytes.')
+            await client.write_event(Event('audio-chunk', audio_metadata, audio_data))
         await client.write_event(Event('audio-stop', audio_metadata))
         result = await client.read_event()
         print(result)
